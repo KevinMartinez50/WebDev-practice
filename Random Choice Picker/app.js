acef8138dd7972bca78ce5aecf193e61ea88eff8
Kevin Martinez <98245796+Bevvo5058@users.noreply.github.com>
2023-08-05 23:55:03 -0500

    Add files via upload

--- Random Choice Picker/app.js
+++ Random Choice Picker/app.js
@@ -47,31 +47,61 @@ function createTags(input) {
   });
 }
 
+// this one is going to be heavy so lets start
 function randomSelect() {
+  // so we add a variable called times and give it a number value
+  // this will be our starting point
+  // so what this will do is the number of times it will highlight
   const times = 30;
 
+  // then we make another variable called interval that will use the setInterval method
+  // setInterval is a method that calls the function at specified intervals or milliseconds
+  //  this will continue until we use a clearInterval function
   const interval = setInterval(() => {
+    // within this setInterval we'll make another variable and equal that to a function pickRandomTag
     const randomTag = pickRandomTag();
+
+    // then we'll get 2 more functions highLight and the unHighlight tag
+    // we use the highlight function with the parameters of randomTag
     highlightTag(randomTag);
+
+    // then we'll set a Timeout this is so we can flick one of the tags on and off
+    // setTimeout() sets a timer which will execute the function or piece of code once time expires
     setTimeout(() => {
+      // then we use unhighlight again for the randomTag
       unHighlightTag(randomTag);
+      // the 100 means the milliseconds so it will activate the function at 100 milliseconds
     }, 100);
   }, 100);
-  setTimeout(() => {
-    clearInterval(interval);
-    setTimeout(() => {
-      const randomTag = pick;
 
+  // so here is where the app decides where to stop
+  // so once again we use the setTimeout
+  setTimeout(() => {
+    // clearInterval is used to stop our setInteval from going on a loop
+    clearInterval(interval);
+    // we make another setTimeout function
+    setTimeout(() => {
+      // within it we make another variable randomTag = to pickRandomTag
+      const randomTag = pickRandomTag();
+
+      // and we highlight randomTag
       highlightTag(randomTag);
     }, 100);
+    // this we multiply the times to 100 (times * interval)
   }, times * 100);
 }
 
+// the pickRandomTag function will be using the Math. methods
 function pickRandomTag() {
+  // make a variable to select the tags
   const tags = document.querySelectorAll(".tag");
+  // Math.floor is a function that will ALWAYS round down to the nearest integer
+  // Math.random() is a function that as it suggest will choose a random number that is greater than or equal to 0 and less than 1
   return tags[Math.floor(Math.random() * tags.length)];
 }
 
+// these 2 are simple functions
+// with the paramaters of tag we want to add the highlight class into it or remove it
 function highlightTag(tag) {
   tag.classList.add("highlight");
 }
